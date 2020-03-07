f928ff47e1e5750dcad161868d869e8285b747aa
Tom <617288330@qq.com>
2020-03-07 11:48:54 +0800

    使用hexo初始化博客

--- README.md
+++
@@ -1,1 +0,0 @@
-# meetfuture.github.io
--- index.js
+++
@@ -1,8 +0,0 @@
-$(function() {
-	$(".MenuButton").bind(PublicJs.Mouse.Up, function() {
-		var PageName = $(this).attr("data-name");
-		var PageUrl = $(this).attr("data-href");
-		var iframe = $(".Frame");
-		iframe.attr("src", PageUrl).attr("data-url", PageUrl).attr("data-name", PageName);
-	})
-})
