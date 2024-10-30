0ea41c7ba784a1c329b18de67c83bc71e0f292bd
Tom <617288330@qq.com>
2024-10-30 23:00:54 +0800

    Site updated: 2024-10-30 23:00:46

--- asserts/js/snow.js
+++ asserts/js/snow.js
@@ -1,9 +1,8 @@
 $(function () {
-    let isMain = $("#body-wrap .full_page .blogtitle").text().trim();
-    /**不是首页则显示雪花效果*/
-    if (!isMain) {
-        for (let i = 0; i < 200; i++) {
-            $("#body-wrap").append("<div class='snow'></div>")
-        }
+  /**不是首页则显示雪花效果*/
+  if (!isIndexPage()) {
+    for (let i = 0; i < 200; i++) {
+      $("#body-wrap").append("<div class='snow'></div>")
     }
+  }
 });
